92e58ecbe45f171812c9ca71c65169c623acf3e2
Grant Morgan <MorganIII.Grant@gmail.com>
2020-05-27 11:35:45 -0400

    stuff

---
+++ src/Components/ResetButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ResetButton = props =>
+    
+    <button
+        className="reset--button"
+        onClick={props.click}>
+        Reset the Game!
+    </button>
+    
+
+    export default ResetButton
